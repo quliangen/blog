dcdc0de3b2149be81748316953a9be66fea4d8fa
quliangen <quliangen@shiqiao.com>
2022-09-23 00:26:27 +0800

    chore: 修改deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:quliangen/blog.git main:gh-pages
+git push -f git@github.com:quliangen/blog.git master:gh-pages
 
 cd -
